e17b06172aca59169265c8ccfed710cb7aa74a60
nick <40447503+nickstanley574@users.noreply.github.com>
2023-03-09 06:50:29 -0600

    changes

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
 
-function getSecondsDownPerYear(uptimePercentage, excludedSeconds) {
+function getSecondsDownPerYear(uptimePercentage, secondsPerYear = 0) {
     const uptime = uptimePercentage / 100;
-    const secondsPerYear = 365 * 24 * 60 * 60;
+    if (secondsPerYear == 0) {
+      secondsPerYear = 365 * 24 * 60 * 60;
+    }
     const secondsDownPerYear = secondsPerYear * (1 - uptime);
     return secondsDownPerYear;
-  }
-  
-  
+}
+
+function letterToNumber(l) {
+    // a  b  c  d  e  f  g  h  i  j  k  l  m  n  o  p  q  r  s  t  u  v  w  x  y  z
+    // 0  1  2  3  4  5  6  7  8  9  10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25  
+    return parseInt(l, 36) - 10
+}
+
+
+
 function convertSecsToDays(seconds) {
     let days = (seconds > 1) ? parseInt(seconds / 86400) : 0;
     return days;
